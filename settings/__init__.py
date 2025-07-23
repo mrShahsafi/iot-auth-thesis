607f72c6c2b6af59c4cb73c74651a17cd8a15e3e
mrshahsafi <xvstanxv@gmail.com>
2025-07-23 15:30:08 +0330

    Init

---
+++ settings/__init__.py
@@ -0,0 +1,3 @@
+from .base import *
+print(f"You are in the {MODE} mode")
+print(f"DIR: {BASE_DIR}")
---
+++ settings/base.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+NUM_NODES = 8
+MSGS_PER_NODE = 15
+ENERGY_PER_BYTE = 0.001
+MQTT_BROKER = "localhost"
+MQTT_PORT = 1883
+TOPIC = "iot/biometric"
+SHARED_SECRET = b'my_shared_secret_key'
+FHE_INTERVAL = 5
+BATTERY_THRESHOLD = 20
+BATTERY_DEFAULT_VALUE = 100
+POLY_MOD_DEGREE = 4096
+
+OUTPUT_FILE = "metrics_log_for_Hybrid_2_new_changesPy.csv"
+REPLAY_WINDOW_SEC = 60
+MODE = "Hybrid"
